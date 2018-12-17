f572dca13b62335e276559baeb72b75bfac8ca34
高城友豪 <a217048@ns.kogakuin.ac.jp>
2018-12-17 19:44:53 +0900

    arrangeの中身をよくしたはず

--- class_item/target_item/arrange_the_point_class.py
+++ class_item/target_item/arrange_the_point_class.py
@@ -6,35 +6,42 @@ class arrange_the_point(object):
     """docstring for arrange_the_point."""
     delta_s = 0
     index = []
+    maxmum_interval = 0
     def __init__(self):
-        #super(arrange_the_point, self).__init__()
         pass
-    def arrange(self, bezier_set_x, bezier_set_y, TVP_of_S):
+    def maximumInterval(self, TVP_of_S):
+        INDEX_OF_TVPS = np.arange(start=0.0, stop=len(TVP_of_S)-1, step=1, dtype= int)
+        for index in INDEX_OF_TVPS
+            if (index-1) <= 0:
+                self.maxmum_interval = TVP_of_S[index]
+            else:
+                interval = TVP_of_S[index] - TVP_of_S[index-1]
+                if self.maxmum_interval<interval:
+                    self.maxmum_interval = interval
+                else:
+                    pass
+    def arrange(self, before_x, before_y, TVP_of_S):
         print('+--+-start arrange-+--+')
         index_for_TVP = 0
         count = 0
-        Ib = np.arange(start=0.0, stop=len(bezier_set_x)-1, step=1, dtype= int)
+        self.maxmumInterval(TVP_of_S)
+        Ib = np.arange(start=0.0, stop=len(before_x)-1, step=1, dtype= int)
         #print('Ib = {}'.format(Ib))
-        for index_for_bezier in Ib:
-            if (index_for_bezier-1) < 0:
-                dx = bezier_set_x[index_for_bezier]
-                dy = bezier_set_y[index_for_bezier]
+        for index in Ib:
+            if (index-1) <= 0:
+                dx = before_x[index]
+                dy = before_y[index]
             else:
-                dx = bezier_set_x[index_for_bezier] - bezier_set_x[index_for_bezier-1]
-                dy = bezier_set_y[index_for_bezier] - bezier_set_y[index_for_bezier-1]
+                dx = before_x[index] - before_x[index-1]
+                dy = before_y[index] - before_y[index-1]
             ds = np.sqrt(dx**2 + dy**2)
             self.delta_s = self.delta_s + ds
             error = TVP_of_S[index_for_TVP] - self.delta_s
-            if error < 0:
-                error = -1.0 * error
-            else:
-                pass
-            if error < 3:
-                self.index.append(index_for_bezier)
+            if error < self.maxmum_interval:
+                self.index.append(index)
                 error = 0.0
                 index_for_TVP = index_for_TVP + 1
                 #print('index_for_TVP = {}'.format(index_for_TVP))
-
                 count = count + 1
                 #print('count = {}'.format(count))
             else:
